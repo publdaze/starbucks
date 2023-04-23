6b1355ac4bf8385eae286ebc84b97506c09aa052
publdaze <publdaze@naver.com>
2023-04-23 15:17:46 +0900

    feat : focus 여부에 따른 placeholder 처리

--- js/main.js
+++ js/main.js
@@ -4,3 +4,11 @@ const searchInputEl = searchEl.querySelector("input");
 searchEl.addEventListener("click", function () {
   searchInputEl.focus();
 });
+
+searchInputEl.addEventListener("focus", function () {
+  searchInputEl.setAttribute("placeholder", "통합검색");
+});
+
+searchInputEl.addEventListener("blur", function () {
+  searchInputEl.setAttribute("placeholder", "");
+});
