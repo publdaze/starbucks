01813c244f22194acf87561c102f4c43c008f9fe
publdaze <publdaze@naver.com>
2023-04-23 15:19:48 +0900

    feat : 인풋 focus 되면 검색 아이콘 안 보이도록 처리

--- js/main.js
+++ js/main.js
@@ -6,9 +6,11 @@ searchEl.addEventListener("click", function () {
 });
 
 searchInputEl.addEventListener("focus", function () {
+  searchEl.classList.add("focused");
   searchInputEl.setAttribute("placeholder", "통합검색");
 });
 
 searchInputEl.addEventListener("blur", function () {
+  searchEl.classList.remove("focused");
   searchInputEl.setAttribute("placeholder", "");
 });
